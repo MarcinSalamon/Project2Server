1478778a55ff50e548b7bd44663c397aa5a33c0a
danielsidhu <40637004+danielsidhu@users.noreply.github.com>
2018-09-12 19:41:33 -0400

    removing src file because update user is broken

--- src/main/java/com/revature/repository/FriendsRepo.java
+++
@@ -1,9 +0,0 @@
-package com.revature.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.revature.beans.FriendsList;
-
-public interface FriendsRepo extends CrudRepository<FriendsList, Integer>{
-
-}
