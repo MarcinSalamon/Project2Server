975243ea1af406b66a036b0a0349ed796117c9c3
MarcinSalamon <Marcin+salamonmarcin29@gmail.com>
2018-09-12 14:19:49 -0400

    purge master for development

--- Project2Server/src/main/java/com/revature/repository/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.revature.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.revature.beans.User;
-
-public interface UserRepo extends CrudRepository<User, Integer>{
-
-}
