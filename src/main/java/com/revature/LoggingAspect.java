d9ce6ed48b0672b1fa4a8e55a2d019b1c938fbdb
MarcinSalamon <Marcin+salamonmarcin29@gmail.com>
2018-09-15 15:18:37 -0400

    log4j imported

--- src/main/java/com/revature/LoggingAspect.java
+++ src/main/java/com/revature/LoggingAspect.java
@@ -1,28 +1,27 @@
 package com.revature;
 
+import org.apache.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 
 @Aspect
 @Configuration
 public class LoggingAspect {
 	
-	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	private Logger logger = Logger.getLogger(this.getClass());
 	
 
 	@Before("execution(* com.revature.*.*(..))")
 	public void before(JoinPoint joinPoint){
 		
-		logger.info("executed: {}", joinPoint);
+		logger.info(joinPoint);
 	}
 
 	@AfterThrowing(pointcut = "execution(* com.revature.*.*(..))", throwing = "t")
 	public void exceptionCatch(JoinPoint jp, Throwable t) {
-		logger.error("{} threw exception {}", jp, t);
+		logger.error(jp, t);
 	}
 }
